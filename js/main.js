7c54100c71e7caa61bfcd37811130d2e447adf9f
KyleKart <71104484+KyleKart@users.noreply.github.com>
2026-05-13 10:47:46 -0300

    dffg

--- js/main.js
+++ js/main.js
@@ -70,8 +70,8 @@ if (params.get('dev') === 'true') {
 }
 
 windowManager.createWindow({
-  title: 'Welcome',
-  content: `<iframe src="./applications/welcome.html" width="100%" height="100%" style="border:none;"></iframe>`,
+  title: 'Beta',
+  content: `<iframe src="./applications/beta.html" width="100%" height="100%" style="border:none;"></iframe>`,
   x: 100,
   y: 100,
   width: 800,
