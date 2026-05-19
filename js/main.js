097b5c81371aaefc57143a7d17fdf6d26aef6956
KyleKart <71104484+KyleKart@users.noreply.github.com>
2026-05-19 19:50:36 -0300

    dfgdhfg

--- js/main.js
+++ js/main.js
@@ -74,8 +74,8 @@ windowManager.createWindow({
   content: `<iframe src="./applications/beta.html" width="100%" height="100%" style="border:none;"></iframe>`,
   x: 500,
   y: 100,
-  width: 650,
-  height: 350
+  width: 700,
+  height: 300
 });
 
 window.addEventListener("message", (event) => {
